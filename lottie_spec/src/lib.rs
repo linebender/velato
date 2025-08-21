e44dd0990e465f7345d80e9a0c6c4b05065711c9
Bilan Vadim <vadimb892@gmail.com>
2025-08-21 10:36:28 +0300

    fix: tests

--- lottie_spec/src/lib.rs
+++ lottie_spec/src/lib.rs
@@ -8,10 +8,12 @@ include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
 #[cfg(test)]
 mod tests {
+    use crate::Composition;
+
     #[test]
     fn parse_lottie() {
-        let content = std::fs::read_to_string("examples/assets/lottie/google.json").unwrap();
+        let content = std::fs::read_to_string("../examples/assets/lottie/google.json").unwrap();
         let v: serde_json::Value = serde_json::from_str(&content).unwrap();
-        let c: Composition = serde_json::from_value(v).unwrap();
+        let _c: Composition = serde_json::from_value(v).unwrap();
     }
 }
