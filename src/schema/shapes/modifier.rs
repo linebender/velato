2800c28ca329660b4b84ac69bd8925767fcb31bb
Robert Borg <hello@robertbrewitz.com>
2025-11-11 18:22:21 +0100

    add copyright notices

--- src/schema/shapes/modifier.rs
+++ src/schema/shapes/modifier.rs
@@ -1,3 +1,6 @@
+// Copyright 2025 the Velato Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 use serde::{Deserialize, Serialize};
 
 use crate::schema::shapes::graphic_element::GraphicElementShape;
