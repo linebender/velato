a10f8fedcd9d8735ed0b648b3e9dd5a0028e7e9c
Nico Burns <nico@nicoburns.com>
2026-01-13 21:21:24 +0000

    Remove  dependency
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,24 @@
 // Copyright 2023 the Velato Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-use thiserror::Error;
-
 /// Triggered when is an issue parsing a lottie file.
-#[derive(Error, Debug)]
+#[derive(Debug)]
 #[non_exhaustive]
 pub enum Error {
-    #[error("Error parsing lottie: {0}")]
-    Json(#[from] serde_json::Error),
+    Json(serde_json::Error),
+}
+
+impl core::error::Error for Error {}
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Json(err) => write!(f, "Error parsing lottie: {err}"),
+        }
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(value: serde_json::Error) -> Self {
+        Self::Json(value)
+    }
 }
