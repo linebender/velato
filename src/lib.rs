35209d07306acc0f3d35b4565f5f1605aa83e398
Nico Burns <nico@nicoburns.com>
2026-01-13 21:09:19 +0000

    Fic rustdoc example
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,29 @@
 // Copyright 2024 the Velato Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Render a Lottie animation to a Vello [`Scene`](crate::vello::Scene).
-//!
-//! This currently lacks support for a [number of important](crate#unsupported-features) SVG features.
-//!
-//! This is also intended to be the preferred integration between Vello and [usvg](https://crates.io/crates/usvg),
-//! so [consider contributing](https://github.com/linebender/vello_svg) if you need a feature which is missing.
-//!
-//! This crate also re-exports [`vello`], so you can easily use the specific version that is compatible with Velato.
+//! Render a Lottie animation to any type that implements the [PaintScene](https://docs.rs/anyrender/latest/anyrender/trait.PaintScene.html)
+//! trait from the [`anyrender`](https://github.com/DioxusLabs/anyrender) crate. There are implementations available for `vello`, `vello_cpu`,
+//! `vello_hybrid`, and `skia_safe`, or you can implement the trait yourself.
 //!
 //! ## Usage
 //!
 //! ```no_run
 //! # use std::str::FromStr;
-//! use velato::vello;
 //!
 //! // Parse your lottie file
 //! let lottie = include_str!("../examples/assets/google_fonts/Tiger.json");
 //! let composition = velato::Composition::from_str(lottie).expect("valid file");
 //!
 //! // Render to a scene
-//! let mut new_scene = vello::Scene::new();
+//! let mut scene = vello::Scene::new();
+//! let mut painter = anyrender_vello::VelloScenePainter::new(&mut scene);
 //!
 //! // Render to a scene!
 //! let mut renderer = velato::Renderer::new();
 //! let frame = 0.0; // Arbitrary number chosen. Ensure it's a valid frame!
 //! let transform = vello::kurbo::Affine::IDENTITY;
 //! let alpha = 1.0;
-//! let scene = renderer.render(&composition, frame, transform, alpha);
+//! let scene = renderer.render(&composition, frame, transform, alpha, &mut painter);
 //! ```
 //!
 //! # Unsupported features
